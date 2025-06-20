3626492a4cf89db255fdbf97c3935b0a8bb8284c
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2025-06-20 07:51:32 +0000

    Fix: remade both task files

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,43 +1,33 @@
 import '6-password.dart';
 
-class User extends Password {
-  String? name;
-  int? age;
-  double? height;
-  int? id;
-  String user_password = '';
+class User {
+    int id = 0;
+    String name = '';
+    int age = 0;
+    double height = 0.0;
+    Password _user_password = new Password(password: '');
 
-  User({id: int, name: String, age: int, height: double, user_password: String})
-      : super(password: user_password) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.height = height;
-    this.user_password = user_password;
-  }
+    User({ int id = 0, String name = '', int age = 0, double height = 0.0, String user_password = '' }) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this._user_password.password = user_password;
+    }
 
-  static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-        id: userJson['id'],
-        name: userJson['name'],
-        age: userJson['age'],
-        height: userJson['height'],
-        user_password: '');
-  }
+    String get user_password => _user_password.password;
 
-  Map toJson() {
-    return {
-      'id': this.id,
-      'name': this.name,
-      'age': this.age,
-      'height': this.height
-    };
-  }
+    set user_password(String new_user_password) {
+        this._user_password.password = new_user_password;
+    }
 
-  @override
-  String toString() {
-    final pass = new Password(password: this.user_password);
+    String toString() => 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${_user_password.isValid()})';
 
-    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}, Password: ${pass.isValid()})';
-  }
+    Map<String, dynamic> toJson() {
+        return { 'id': this.id, 'name': this.name, 'age': this.age, 'height': this.height };
+    }
+
+    static User fromJson(Map<dynamic, dynamic> userJson) {
+        return new User(id: userJson['id'], name: userJson['name'], age: userJson['age'], height: userJson['height'], user_password: userJson['user_password']);
+    }
 }
