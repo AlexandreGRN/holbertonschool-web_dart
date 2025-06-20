02eaeec15a28ff4493fbc394159be979809ba85d
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2025-06-20 07:50:33 +0000

    Fix: changed tasks  accordingly

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -1,21 +1,32 @@
 class Password {
-  String password = '';
+    String password = '';
 
-  bool isValid() {
-    if (this.password.length >= 8 && this.password.length <= 16) {
-      if (this.password.contains(RegExp(r'[A-Z]')) &&
-          this.password.contains(RegExp(r'[a-z]'))) {
-        if (this.password.contains(RegExp(r'[0-9]'))) {
-          return true;
+    bool isValid() {
+        if (password.length <= 8 || password.length >= 16)
+            return false;
+
+        int $A = 'A'.codeUnitAt(0);
+        int $Z = 'Z'.codeUnitAt(0);
+        int $a = 'a'.codeUnitAt(0);
+        int $z = 'z'.codeUnitAt(0);
+        int $0 = '0'.codeUnitAt(0);
+        int $9 = '9'.codeUnitAt(0);
+
+        bool containsUpper = false;
+        bool containsLower = false;
+        bool containsDigit = false;
+
+        for (int codeUnit in password.codeUnits) {
+            if ($A <= codeUnit && codeUnit <= $Z)
+                containsUpper = true;
+            if ($a <= codeUnit && codeUnit <= $z)
+                containsLower = true;
+            if ($0 <= codeUnit && codeUnit <= $9)
+                containsDigit = true;
         }
-      }
+
+        return containsUpper && containsLower && containsDigit;
     }
 
-    return false;
-  }
-
-  @override
-  String toString() {
-    return 'Your Password is: ${this.password}';
-  }
+    String toString() => 'Your Password is: $password';
 }
